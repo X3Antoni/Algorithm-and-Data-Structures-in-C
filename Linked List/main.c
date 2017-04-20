9190f1ca80686505516acefb71eabf9e2f061d04
Markus Antoni Suwignyo <marktony.x3.24@gmail.com>
2017-04-20 08:57:49 +0700

    Update main.c

--- Linked List/main.c
+++ Linked List/main.c
@@ -6,32 +6,6 @@
 
 #define MAXNUMBER 10
 
-// int *randomList(int min, int max)
-// {
-//     int i;
-//     int r;
-//     int *res = (int *) malloc(sizeof(int) * (max - min));
-//     int *visited = (int *) malloc(sizeof(int) * (max - min));
-
-//     for(i = 0; i < (max - min); i++)
-//         visited[i] = 0;
-    
-//     srand((unsigned) time(NULL));
-
-//     for(i = 0; i < (max - min); i++)
-//     {
-//         r = rand() % (max - min);
-//         if(visited[r] == 0) {
-//             res[i] = r;
-//             visited[r] = 1;
-//         } else {
-//             i--;
-//         }
-//     }
-
-//     return res;
-// }
-
 int main()
 {
     int i;
